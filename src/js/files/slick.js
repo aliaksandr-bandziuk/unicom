a0dca0ece55a7854524c2630f46a41617b7f8c1b
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-09-06 16:50:13 +0300

    create news section

--- src/js/files/slick.js
+++ src/js/files/slick.js
@@ -25,3 +25,31 @@ $(".single-item").slick({
     },
   ]
 });
+
+$(".news__slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // rows: 2,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    },
+  ]
+})
