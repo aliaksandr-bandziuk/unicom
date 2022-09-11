d51d1b4b31819f0974cb044fbc7a75a0b913c1bb
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-09-11 20:46:02 +0300

    add some changes

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -22,11 +22,11 @@ import "../../scss/base/swiper.scss";
 // import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
 // import 'swiper/css';
-
 // Инициализация слайдеров
 function initSliders() {
+
 	// Перечень слайдеров
-	// Проверяем, есть ли слайдер на стронице
+	// Проверяем, есть ли слайдер на странице
 	if (document.querySelector('.swiper-main')) { // Указываем скласс нужного слайдера
 		// Создаем слайдер
 		new Swiper('.swiper-main', { // Указываем скласс нужного слайдера
@@ -101,6 +101,93 @@ function initSliders() {
 			}
 		});
 	}
+		// Проверяем, есть ли слайдер на странице
+		if (document.querySelector('.swiper-range')) { // Указываем скласс нужного слайдера
+			// Создаем слайдер
+			new Swiper('.swiper-range', { // Указываем скласс нужного слайдера
+				// Подключаем модули слайдера
+				// для конкретного случая
+				modules: [Navigation, Pagination, EffectFade, Autoplay],
+				observer: true,
+				observeParents: true,
+				slidesPerView: 1,
+				spaceBetween: 0,
+				autoHeight: true,
+				speed: 800,
+	
+				//touchRatio: 0,
+				//simulateTouch: false,
+				loop: true,
+				//preloadImages: false,
+				//lazy: true,
+				
+				// Эффекты
+				// effect: 'fade',
+				// autoplay: {
+				// 	delay: 3000,
+				// 	disableOnInteraction: false,
+				// },
+	
+				// Пагинация
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+	
+				// Скроллбар
+				/*
+				scrollbar: {
+					el: '.swiper-scrollbar',
+					draggable: true,
+				},
+				*/
+	
+				// Кнопки "влево/вправо"
+				navigation: {
+					prevEl: '.swiper-button-prev',
+					nextEl: '.swiper-button-next',
+				},
+	
+				// Брейкпоинты
+				breakpoints: {
+					// 0: {
+					// 		slidesPerView: 1,
+					// },
+					350: {
+							slidesPerView: 1,
+					},
+					750: {
+							slidesPerView: 2,
+							spaceBetween: 48
+					}
+				},
+				/*
+				breakpoints: {
+					320: {
+						slidesPerView: 1,
+						spaceBetween: 0,
+						autoHeight: true,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 20,
+					},
+					992: {
+						slidesPerView: 3,
+						spaceBetween: 20,
+					},
+					1268: {
+						slidesPerView: 4,
+						spaceBetween: 30,
+					},
+				},
+				*/
+				// События
+				on: {
+	
+				}
+			});
+		}
 
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
